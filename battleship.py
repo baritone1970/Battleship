e67bca121bf0bb1033dc6194615665ab3f171ab7
Dmitry Sidorov <53134123+baritone1970@users.noreply.github.com>
2022-04-22 00:37:18 +0300

    Update battleship.py
    Почти работает! Можно вставить одноклеточный корабль!

--- battleship.py
+++ battleship.py
@@ -82,8 +82,8 @@ class Ship:
             raise ShipException("Invalid ship size error", size)
 
     def set_hit(self):
-        if lives > 0:
-            lives -= 1
+        if self.lives > 0:
+            self.lives -= 1
 
     def set_place(self, place):  # Думаю, этот метод лучше иметь в классе Board, но посмотрим
         pass
@@ -94,25 +94,22 @@ class Board:
     # Корабли должны находится на расстоянии минимум одна клетка друг от друга.
     # Доска вмещает 1 корабль на 3 клетки, 2 корабля на 2 клетки, 4 корабля на одну клетку.
 
-    map = []  # Будет содержать карту точек кораблей, список строк (списков точек).
-    # Номер элемента строки (колонка c) вводится буквой, номер строки r - цифрой
-    # Помни, студент: в координате map[r][c] сначала номер строки, потом - номер колонки
-    free_dots = []  # Список свободных точек, используется лишь для ускорения размещения кораблей
-    ship_list = []   # список кораблей
-
     def __init__(self, hidenboard=False):
+        self.map = []  # Будет содержать карту точек кораблей, список строк (списков точек).
+        # Номер элемента строки (колонка c) вводится буквой, номер строки r - цифрой
+        # Помни, студент: в координате map[r][c] сначала номер строки, потом - номер колонки
         for r in range(6):  # Точки на карте при создании не принадлежат никаким кораблям
             if hidenboard:
                 self.map.append([DotOnHidenBoard() for d in range(6)])
             else:
                 self.map.append([Dot() for d in range(6)])
-        for r in range(6):  # Заполняем список свободных для размещения кораблей точек [0-5][0-5]
+        self.free_dots = set()  # Множество свободных точек, используется лишь для ускорения размещения кораблей
+        for r in range(6):  # Заполняем множество свободных для размещения кораблей точек [0-5][0-5]
             for c in range(6):
-                self.free_dots.append((r, c))  # self.map[c][r]
+                self.free_dots.add((r, c))  # self.map[r][c]
+        self.ship_list = []  # список кораблей
         for s in [1, 1]:  # список размеров размещаемых кораблей по порядку от 3 до 1
             self.ship_list.append(Ship(s))
-        print(self.ship_list)
-
 
     def coord_in_map(self, place):
         # Положение вводится строкой, буква (колонка c) и цифра (ряд r)
@@ -127,6 +124,10 @@ class Board:
         # Положение вводится строкой, буква и цифра, направление - символами 'U', 'R', 'D', 'L'
         r, c = place
         self.map[r][c].set_ship(ship)  # пока никаких проверок.
+        for dr in [-1, 0, 1]:       # Удаляем из свободных точку под кораблём и воокруг него
+            for dc in [-1, 0, 1]:
+                self.free_dots.discard((r + dr, c + dc))
+        print(self.free_dots)
 
     def place_all_ships(self, auto=False):
         for ship in self.ship_list:  # список размеров размещаемых кораблей по порядку от 3 до 1
@@ -177,6 +178,7 @@ class Game:
         self.user_board = Board()  # Доска пользователя
         self.ai = Ai()  # Игровая механика компьютера
         self.ai_board = Board(hidenboard=True)
+        # self.ai_board.map[2][4].ship=Ship(1)    #TODO
 
     def random_board(self, board):
         # Здесь на доске генерится случайное расположение кораблей, начиная с самого большого
@@ -197,6 +199,7 @@ class Game:
         print('''Сейчас мы расставим ваши корабли. Координаты вводятся как в шахматах: 
 сначала - буква, потом - цифра, без пробела. Например, "e2", "e4" :-)''')
         self.user_board.place_all_ships()
+        # self.ai_board.map[2][4].hit=True
         self.printboards()
 
     def loop(self):  # Это цикл хода игры
